83bc8e9d3c9cc2358aaae5c22d6703a79c51aade
eltonk2k <elieltonpb1@gmail.com>
2020-04-13 12:23:17 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,31 +14,39 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ChessMatch chessMath = new ChessMatch();
+		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
 		
-		while(!chessMath.getCheckMate()) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMath, captured);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
-				boolean[][] possibleMoves = chessMath.possibleMoves(source);
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
-				UI.printBoard(chessMath.getPieces(), possibleMoves);
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
 				System.out.println();
 				System.out.print("Tanget: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece capturedPiece = chessMath.performChessMove(source, target);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
+				
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
@@ -51,7 +59,7 @@ public class Program {
 		}
 	
 		UI.clearScreen();
-		UI.printMatch(chessMath, captured);
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
